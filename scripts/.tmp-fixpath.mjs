48ae79e1ec253eb08fd2d44f37f5e7bc4b1131e1
tanviruchahs2580 <tanviruchahs2580@users.noreply.github.com>
2026-08-28 20:16:43 +0600

    chore: remove temp debug scripts (gitleaks) + gitignore .tmp-*

--- scripts/.tmp-fixpath.mjs
+++
@@ -1,5 +0,0 @@
-import fs from "node:fs";
-let s = fs.readFileSync("scripts/.tmp-govdbg.mjs", "utf8");
-s = s.split('"./apps/').join('"../apps/');
-fs.writeFileSync("scripts/.tmp-govdbg.mjs", s);
-console.log("paths fixed");
